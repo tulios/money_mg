a30dd5dba8356191293fecddecf3ece2a9ad9298
tulios <ornelas.tulio@gmail.com>
2009-10-02 20:38:13 -0300

    Correcao em um require.

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
            
 # Métodos formatadores
-require 'lib/utils/formater'
+require "#{File.dirname(__FILE__)}/../lib/utils/formater"
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
